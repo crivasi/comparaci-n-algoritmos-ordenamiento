e98b69d768dfa9ef8476767dd5ec880831e64c91
Christian Rivas <cfrivas22@hotmail.com>
2022-03-30 11:21:39 -0500

    Agregando heapsort mejorado - Cambiando el heapsort

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ const mensajeVectorGenerado = document.getElementById('mensaje-vector-generado')
 const mensajeVectorCopiado = document.getElementById('mensaje-vector-copiado');
 const divMuestraVector = document.getElementById('muestra-vector-generado');
 
-const colIntercambiosQuicksort = document.querySelector('#quicksort-intercambios');
+const colIntercambiosBurbuja = document.querySelector('#burbuja-intercambios');
+const colComparacionesBurbuja = document.querySelector('#burbuja-comparaciones');
 const colIntercambiosHeap = document.querySelector('#heap-intercambios');
+const colComparacionesHeap = document.querySelector('#heap-comparaciones');
+const colIntercambiosHeap2 = document.querySelector('#heap-intercambios2');
+const colComparacionesHeap2 = document.querySelector('#heap-comparaciones2');
+const colIntercambiosHeapMejorado = document.querySelector('#heap-mejorado-intercambios');
+const colComparacionesHeapMejorado = document.querySelector('#heap-mejorado-comparaciones');
 
 const vectorAleatorio = [];
 
@@ -28,8 +34,14 @@ radiosSizeVector.forEach(function (radioSize) {
 });
 
 function resetValues() {
+  intercambiosBurbuja = 0;
+  comparacionesBurbuja = 0;
   intercambiosHeap = 0;
-  intercambiosQuicksort = 0;
+  comparacionesHeap = 0;
+  intercambiosHeap2 = 0;
+  comparacionesHeap2 = 0;
+  intercambiosHeapMejorado = 0;
+  comparacionesHeapMejorado = 0;
 }
 
 function desaparecerMensajeCopiado() {
@@ -64,39 +76,97 @@ function copiarVectorGenerado() {
 }
 
 function ordenarVector() {
+  resetValues();
   btnOrdenarVector.setAttribute('disabled', '');
 
-  const vectorQuicksort = [...vectorAleatorio];
+  const vectorBurbuja = [...vectorAleatorio];
   const vectorHeapsort = [...vectorAleatorio];
+  const vectorHeapsort2 = [...vectorAleatorio];
+  const vectorHeapsortMejorado = [...vectorAleatorio];
 
-  quickSort(vectorQuicksort, 0, vectorQuicksort.length - 1);
+  bubbleSort(vectorBurbuja);
   heapSort(vectorHeapsort);
+
+  heapsort2(vectorHeapsort2);
+  heapsortEnhace(vectorHeapsortMejorado);
+
   agregarDatosTabla();
 
   btnOrdenarVector.removeAttribute('disabled');
-  
-  divMuestraVector.textContent = `${JSON.stringify(vectorHeapsort).substring(0,500)}...`;
+
+  divMuestraVector.textContent = `${JSON.stringify(vectorHeapsortMejorado).substring(0,500)}...`;
 }
 
 function agregarDatosTabla() {
-  colIntercambiosQuicksort.textContent = intercambiosQuicksort;
+  colIntercambiosBurbuja.textContent = intercambiosBurbuja;
+  colComparacionesBurbuja.textContent = comparacionesBurbuja;
   colIntercambiosHeap.textContent = intercambiosHeap;
+  colComparacionesHeap.textContent = comparacionesHeap;
+  colIntercambiosHeap2.textContent = intercambiosHeap2;
+  colComparacionesHeap2.textContent = comparacionesHeap2;
+  colIntercambiosHeapMejorado.textContent = intercambiosHeapMejorado;
+  colComparacionesHeapMejorado.textContent = comparacionesHeapMejorado;
 }
 
 ///////////////////// HEAPSORT ///////////////
-let comparacionesHeap = 0;
-let intercambiosHeap = 0;
+let intercambiosHeap2 = 0;
+let comparacionesHeap2 = 0;
+
+const getBigger = (data, bigger, toCompare) => {
+  comparacionesHeap2++;
+  return data[bigger] >= data[toCompare] ? bigger : toCompare;
+}
+ 
+const maxHeapify = (data, rootIndex, length) => {
+  const leftIndex = rootIndex * 2 + 1;
+  const rightIndex = rootIndex * 2 + 2;
+  let bigger = rootIndex;
+ 
+  comparacionesHeap2 += 3;
+
+  if (leftIndex <= length) {
+    bigger = getBigger(data, bigger, leftIndex);
+  }
+
+  if (rightIndex <= length) {
+    bigger = getBigger(data, bigger, rightIndex);
+  }
+  
+  if (bigger !== rootIndex) {
+    intercambiosHeap2++;
+    [data[rootIndex], data[bigger]] = [data[bigger], data[rootIndex]];
+    maxHeapify(data, bigger, length);
+  }
+};
+ 
+const heapsort2 = data => {
+  const half = Math.floor(data.length / 2);
+  for (let rootIndex = half; rootIndex >= 0; rootIndex--) {
+    comparacionesHeap2++;
+    maxHeapify(data, rootIndex, data.length - 1);
+  }
+  
+  for (let length = data.length - 1; length >= 0; length--) {
+    intercambiosHeap2++;
+    [data[0], data[length]] = [data[length], data[0]];
+    maxHeapify(data, 0, length - 1);
+  }
+  
+  return data;
+};
+
+//////////
 
 var array_length;
 
 // A utility function to swap two elements
 function swapHeap(input, index_A, index_B) {
+  intercambiosHeap++;
+
   var temp = input[index_A];
 
   input[index_A] = input[index_B];
   input[index_B] = temp;
-
-  intercambiosHeap += 1;
 }
 
 /* to create MAX  array */  
@@ -105,11 +175,13 @@ function heapRoot(input, i) {
   var right = 2 * i + 2;
   var max = i;
 
+  comparacionesHeap += 3;
+
   if (left < array_length && input[left] > input[max]) {
     max = left;
   }
 
-  if (right < array_length && input[right] > input[max])     {
+  if (right < array_length && input[right] > input[max]) {
     max = right;
   }
 
@@ -123,82 +195,98 @@ function heapSort(input) {
   array_length = input.length;
 
   for (var i = Math.floor(array_length / 2); i >= 0; i -= 1) {
+    comparacionesHeap++;
     heapRoot(input, i);
   }
 
   for (i = input.length - 1; i > 0; i--) {
-      swapHeap(input, 0, i);
-      array_length--;
-      heapRoot(input, 0);
+    comparacionesHeap++;
+
+    swapHeap(input, 0, i);
+    array_length--;
+    heapRoot(input, 0);
   }
 }
 
 /////////////////////////////
 
+///////// BURBUJA ////////////
+let intercambiosBurbuja = 0;
+let comparacionesBurbuja = 0;
 
-/////////// QUICKSORT ///////////////////
-// A utility function to swap two elements
-function swapQuickSort(input, index_A, index_B) {
-  var temp = input[index_A];
+function bubbleSort(arr) {
+  const length = arr.length;  
+  for (let i = 0; i < length; i++) {
+    comparacionesBurbuja++;
 
-  input[index_A] = input[index_B];
-  input[index_B] = temp;
+    for (let j = 0; j < (length - i - 1); j++) {
+      comparacionesBurbuja++;
 
-  intercambiosQuicksort += 1;
-}
-/* This function takes last element as pivot, places
- the pivot element at its correct position in sorted
- array, and places all smaller (smaller than pivot)
- to left of pivot and all greater elements to right
- of pivot */
-function partition(arr, low, high) {
+      if(arr[j] > arr[j+1]) {
+        const tmp = arr[j]; 
+        intercambiosBurbuja++;
 
-  // pivot
-  let pivot = arr[high];
-
-  // Index of smaller element and
-  // indicates the right position
-  // of pivot found so far
-  let i = (low - 1);
-
-  for (let j = low; j <= high - 1; j++) {
-
-      // If current element is smaller
-      // than the pivot
-      if (arr[j] < pivot) {
-
-          // Increment index of
-          // smaller element
-          i++;
-          swapQuickSort(arr, i, j);
+        arr[j] = arr[j+1]; 
+        arr[j+1] = tmp; 
       }
-  }
-
-  swapQuickSort(arr, i + 1, high);
-
-  return (i + 1);
-}
-
-/* The main function that implements QuickSort
-        arr[] --> Array to be sorted,
-        low --> Starting index,
-        high --> Ending index
-*/
-
-let intercambiosQuicksort = 0;
-
-function quickSort(arr, low, high) {
-  if (low < high) {
-
-      // pi is partitioning index, arr[p]
-      // is now at right place
-      let pi = partition(arr, low, high);
-
-      // Separately sort elements before
-      // partition and after partition
-      quickSort(arr, low, pi - 1);
-      quickSort(arr, pi + 1, high);
+    }        
   }
 }
 
-// This code is contributed by Saurabh Jaiswal
+/////////// HEAPSORT MEJORADO //////////////
+let intercambiosHeapMejorado = 0;
+let comparacionesHeapMejorado = 0;
+
+const getBiggerEnhace = (data, bigger, toCompare) => {
+  comparacionesHeapMejorado++;
+  return data[bigger] >= data[toCompare] ? bigger : toCompare;
+}
+ 
+const maxHeapifyEnhace = (data, rootIndex, length) => {
+  const leftOneIndex = rootIndex * 4 + 1;
+  const leftTwoIndex = rootIndex * 4 + 2;
+  const rightOneIndex = rootIndex * 4 + 3;
+  const rightTwoIndex = rootIndex * 4 + 4;
+  let bigger = rootIndex;
+  
+  comparacionesHeapMejorado += 5;
+
+  if (leftOneIndex <= length) {
+    bigger = getBiggerEnhace(data, bigger, leftOneIndex);
+  }
+
+  if (leftTwoIndex <= length) {
+    bigger = getBiggerEnhace(data, bigger, leftTwoIndex);
+  }
+
+  if (rightOneIndex <= length) {
+    bigger = getBiggerEnhace(data, bigger, rightOneIndex);
+  }
+
+  if (rightTwoIndex <= length) {
+    bigger = getBiggerEnhace(data, bigger, rightTwoIndex);
+  }
+  
+  if (bigger !== rootIndex) {
+    intercambiosHeapMejorado++;
+    [data[rootIndex], data[bigger]] = [data[bigger], data[rootIndex]];
+    maxHeapifyEnhace(data, bigger, length);
+  }
+};
+ 
+const heapsortEnhace = data => {
+  const half = Math.floor(data.length / 4);
+  for (let rootIndex = half; rootIndex >= 0; rootIndex--) {
+    comparacionesHeapMejorado++;
+    maxHeapifyEnhace(data, rootIndex, data.length - 1);
+  }
+  
+  for (let length = data.length - 1; length >= 0; length--) {
+    comparacionesHeapMejorado++;
+    intercambiosHeapMejorado++;
+    [data[0], data[length]] = [data[length], data[0]];
+    maxHeapifyEnhace(data, 0, length - 1);
+  }
+  
+  return data;
+};
